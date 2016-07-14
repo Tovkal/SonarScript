1db0dd2e58ba7b4b1c18156eb27763d90f9312ae
Andrés Pizá <andres.piza@sm2baleares.es>
2016-07-14 09:36:45 +0200

    Initial commit

---
+++ sonar.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    exit 0
+fi
+
+SKIPCOVERAGE=false
+BRANCH=""
+FRONT=false
+
+while [[ $# > 0 ]]
+do
+	key="$1"
+
+	case $key in
+		-sc|--skipcoverage)
+		SKIPCOVERAGE=true
+		;;
+		-b|--branch)
+		BRANCH="$2"
+		;;
+		-f|--front)
+		FRONT=true
+		;;
+		-h|--help)
+		echo "$(basename "$0") [-h] [-sc -b] BRANCH_NAME"
+		echo
+		echo "Calling $(basename "$0") BRANCH_NAME will do a full analysis, including coverage."
+		echo
+		echo "The following flags can also be used:"
+		echo
+		echo "	-sc, --skipcoverage		skip coverage analysis"
+		echo "	-b, --branch			branch to analyze"
+		exit 0
+		;;
+		--default)
+		;;
+		*)
+		BRANCH=$1
+			## unknown option
+		;;
+	esac
+	shift
+done
+
+if [ -z "$BRANCH" ]; then
+	echo "A branch name must be supplied either using the -b flag: 	$(basename $0) -b BRANCH_NAME"
+	echo "or directly after the command: 					$(basename $0) BRANCH_NAME"
+	exit 0
+fi
+
+
+# Step 1: Clean install
+echo "Starting clean install"
+echo
+
+mvn clean install
+if [ "$?" -ne 0 ]; then
+	echo "Clean install unsuccessful"
+	exit 1
+fi
+
+# Step 2: Coverage
+if [[ "$SKIPCOVERAGE" = false ]]; then
+
+	echo "Doing coverage analysis"
+	echo
+
+	mvn cobertura:cobertura -Dcobertura.report.format=XML
+	if [ "$?" -ne 0 ]; then
+	        echo "Coverage analysis unsuccessful"
+	        exit 1
+	fi
+fi
+
+
+# Step 3: Sonar
+echo "Starting sonar build"
+echo
+
+mvn sonar:sonar -Dsonar.branch="$BRANCH"
+if [ "$?" -ne 0 ]; then
+        echo "Sonar build unsuccessful"
+        exit 1
+fi
+
